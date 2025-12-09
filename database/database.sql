3f13adc68189da74d1b8afe482d7d52f93bddae2
CharleryM <charlerymalcolm.l@gmail.com>
2025-12-09 16:18:39 +0100

    mise en place des contners dockers

---
+++ database/database.sql
@@ -0,0 +1,8 @@
+import mysql from "mysql2";
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST, // "mysql"
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
