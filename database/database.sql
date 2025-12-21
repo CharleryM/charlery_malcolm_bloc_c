5c3116304b26bec0a4a736c417a3a90bf930b135
CharleryM <charlerymalcolm.l@gmail.com>
2025-12-21 16:53:29 +0100

    creat users and walette tables

--- database/database.sql
+++ database/database.sql
@@ -6,3 +6,22 @@ const db = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE wallet (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,     -- positif = gain, négatif = perte
+    type ENUM('gain', 'loss') NULL,    -- optionnel, selon tes besoins
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    
+    CONSTRAINT fk_wallet_user
+        FOREIGN KEY (user_id) REFERENCES users(id)
+        ON DELETE CASCADE
+);
