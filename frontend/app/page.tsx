334c98a24d01500f05ab2120354c7969100f0d1c
CharleryM <charlerymalcolm.l@gmail.com>
2025-12-21 14:16:29 +0100

    init principal page and form page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,23 +1,25 @@
 import styles from "../styles/home.module.css";
+import { getAsset} from "@lib/api";
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+export default function Page() {
   return (
     <div className={styles.container}>
       {/* Header */}
       <header className={styles.header}>
-        <a href="./" className={styles.logo}>
+        <a href="/" className={styles.logo}>
           <Image
-            src='/images/logo_digital_wallet.png'
-            alt='Digital Wallet Logo'
+            src="/images/logo_digital_wallet.png"
+            alt="Digital Wallet Logo"
             width={75}
             height={75}
           />
         </a>
         <button className={styles.logo}>
           <Image
-            src='/images/menu.png'
-            alt='Digital Wallet Logo'
+            src="/images/menu.png"
+            alt="Digital Wallet Logo"
             width={50}
             height={50}
           />
@@ -34,29 +36,35 @@ export default function Home() {
         {/* Assets */}
         <section className={styles.assets}>
           <h2>Assets</h2>
+          <ul>
+            <li>
+              {[1, 2, 3, 4, 5].map((_, i) => (
+                <div key={i} className={styles.asset}>
+                  <div className={styles.assetLeft}>
+                    <div className={styles.ethIcon}></div>
+                    <div>
+                      <strong>Ethereum</strong>
+                      <p>1.02 ETH</p>
+                    </div>
+                  </div>
 
-          {[1, 2, 3].map((_, i) => (
-            <div key={i} className={styles.asset}>
-              <div className={styles.assetLeft}>
-                <div className={styles.ethIcon}>◇</div>
-                <div>
-                  <strong>Ethereum</strong>
-                  <p>1.02 ETH</p>
+                  <div className={styles.assetRight}>
+                    <strong>$404.18</strong>
+                    <p className={styles.positive}>+ $24.50</p>
+                  </div>
                 </div>
-              </div>
-
-              <div className={styles.assetRight}>
-                <strong>$404.18</strong>
-                <p className={styles.positive}>+ $24.50</p>
-              </div>
-            </div>
-          ))}
+              ))}
+            </li>
+          </ul>
         </section>
 
         {/* Actions */}
         <footer className={styles.actions}>
-          <button className={styles.deposit}>Deposit</button>
-          <button className={styles.send}>Send Money</button>
+          <button className={styles.send}>
+            <Link href="/transaction">
+              Send Money
+            </Link>
+          </button>
         </footer>
       </div>
     </div>
