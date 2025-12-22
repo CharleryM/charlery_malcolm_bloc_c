d25b4b067230496df3c66fb2cf8a1938e7042b08
CharleryM <charlerymalcolm.l@gmail.com>
2025-12-22 11:04:01 +0100

    set model user and walette

---
+++ backend/src/models/userModel.js
@@ -0,0 +1,19 @@
+import { db } from "../config/db.js";
+
+export async function createUser(name, email, password) {
+  const [result] = await db.query(
+    "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+    [name, email, password]
+  );
+  return result.insertId; // retourne l'id du nouvel user
+}
+
+export async function getAllUsers() {
+  const [rows] = await db.query("SELECT id, name, email FROM users");
+  return rows; // retourne un tableau d'objets
+}
+
+export async function getUserById(id) {
+  const [rows] = await db.query("SELECT * FROM users WHERE id = ?", [id]);
+  return rows[0];
+}
